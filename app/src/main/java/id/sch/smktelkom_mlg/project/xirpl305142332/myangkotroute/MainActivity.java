02b5f786eef0c406d394d584f2de5d4509279228
Shelin <skusmiawati@gmail.com>
2016-11-10 14:56:55 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl305142332/myangkotroute/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl305142332.myangkotroute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
