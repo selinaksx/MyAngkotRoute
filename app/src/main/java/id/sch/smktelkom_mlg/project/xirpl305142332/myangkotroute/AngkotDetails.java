a03c7e817f89d8db11c3789726b832140c3a4f6d
Bagas Try Ambodo <bagasambodo@gmail.com>
2016-11-23 09:05:18 +0700

    repair

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl305142332/myangkotroute/AngkotDetails.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl305142332/myangkotroute/AngkotDetails.java
@@ -3,17 +3,28 @@ package id.sch.smktelkom_mlg.project.xirpl305142332.myangkotroute;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class AngkotDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_angkot_details);
         Intent intent = getIntent();
 
         setTitle(intent.getStringExtra("angkot"));
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
     
 }
