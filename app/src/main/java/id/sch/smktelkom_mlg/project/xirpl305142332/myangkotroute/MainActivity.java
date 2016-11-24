b7d4364f07f12a0602f274cf368448016d785f2c
Ifan Rifa'i Hardiyanto <ifanrifai24@gmail.com>
2016-11-24 13:29:11 +0700

    RouteActivity

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl305142332/myangkotroute/MainActivity.java
@@ -0,0 +1,30 @@
+package id.sch.smktelkom_mlg.project.xirpl305142332.myangkotroute;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by ASUS TP 450 LDV on 11/24/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("My Angkot Route");
+        findViewById(R.id.buttonAngkot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AngkotList.class));
+            }
+        });
+        findViewById(R.id.buttonRoute).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RouteActivity.class));
+            }
+        });
+    }
+}
